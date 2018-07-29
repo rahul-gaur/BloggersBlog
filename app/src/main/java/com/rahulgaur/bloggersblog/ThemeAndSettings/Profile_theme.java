4ca9383bbb8a8f6fbde23a85ab4e55ad93236d9e
Rahul Gaur <rahul.gaur152@gmail.com>
2018-07-29 09:40:26 +0530

    tweaking previous changes

---
+++ app/src/main/java/com/rahulgaur/bloggersblog/ThemeAndSettings/Profile_theme.java
@@ -0,0 +1,15 @@
+package com.rahulgaur.bloggersblog.ThemeAndSettings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.rahulgaur.bloggersblog.R;
+
+public class Profile_theme extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_theme);
+    }
+}
